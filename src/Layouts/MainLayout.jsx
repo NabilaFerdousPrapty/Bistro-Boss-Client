ad0c0f2500c3a1a33325cc38ea8736013402fa20
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-05-24 21:35:55 +0600

    login sign up page done

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,17 +1,24 @@
 
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 import Footer from '../Pages/Shared/Footer/Footer';
 
 const MainLayout = () => {
+     const location=useLocation();
+     const noHeaderFooter=location.pathname.includes('login') || location.pathname.includes('signup');
     return (
         <div>
             <div className='max-w-7xl mx-auto'>
-            <Navbar/>
+                {
+                    noHeaderFooter||<Navbar/>
+                }
+            
             <Outlet/>
             </div>
             
-            <Footer/>
+            {
+                noHeaderFooter||<Footer/>
+            }
         </div>
     );
 };
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -5,6 +5,8 @@ import ContactUs from "../Pages/ContactUs/ContactUs";
 import OurShop from "../Pages/OurShop/OurShop";
 import Menu from "../Pages/Menu/Menu";
 import Dashboard from "../Pages/DASHBOARD/Dashboard";
+import Login from './../Pages/Login/Login';
+import SignUp from "../Pages/SignUp/SignUp";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -25,6 +27,12 @@ const router = createBrowserRouter([
         },{
           path:'/dashboard',
           element: <Dashboard/>
+        },{
+          path:'/login',
+          element:<Login/>
+        },{
+          path:'/signup',
+          element:<SignUp/>
         }
         
     ]
